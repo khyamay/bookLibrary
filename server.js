e4bccaff9146ddb69d9b1939575ff9a0f9018821
Khemraj Thapa <khyamay@gmail.com>
2014-01-31 20:12:47 +0000

    added rest api

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/api', function(request, response){
 });
 
 //connect to database
-mongoose.connect('mongodb://127.0.0.1:27017');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 //schemas
 var Book = new mongoose.Schema({
@@ -80,6 +80,44 @@ app.get('/api/books/:id', function(request, response){
 		}
 	});
 });
+
+//Insert a new book
+app.post('/api/books', function(request, response){
+	var book = new BookModel({
+		title: request.body.title,
+		author: request.body.author,
+		releaseDate: request.body.releaseDate
+	});
+	book.save(function(err){
+		if(!err){
+			return console.log('created');
+		}else{
+			return console.log(err);
+		}
+	});
+	return response.send(book);
+
+})
+
+//Update a book 
+app.put('/api/books:id', function(request, response){
+	console.lgo('Updating book '+ request.body.title);
+	return BookModel.findById(request.params.id, function(err, book){
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate;
+
+		return book.save(function(err){
+			if(!err){
+				console.log('book updated');
+			}
+			else{
+				console.log(err);
+			}
+			return response.send(book);
+		})
+	})
+})
 
 //Start server
 var port = 4711;
